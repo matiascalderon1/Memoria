0ac0bb26cb38bcc960ae744c7bd25cb3dcbf3b63
matiascalderon1 <matiascalderon@iMac-de-Matias.local>
2016-10-18 22:00:46 -0300

    Configuracion rutas v2

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var skills = require('./routes/skills');
+var work = require('./routes/work');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/skills', skills);
+app.use('/work', work);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
